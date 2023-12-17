4de2103087aa08e5e1af2ac6f397b445d76ccd94
Alina <alinasy245@gmail.com>
2023-12-18 00:33:51 +0200

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+const galleryMarkup = galleryItems.map(item => createGalleryItem(item)).join('');
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+});
+
+document.addEventListener('keydown', (event) => {
+  const instance = basicLightbox.get();
+  if (event.key === 'Escape' && instance) {
+    instance.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+const galleryMarkup = galleryItems.map(item => createGalleryItem(item)).join('');
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
